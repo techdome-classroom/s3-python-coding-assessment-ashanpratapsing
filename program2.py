c220870875dcc201df55014cd408395bd49aba22
ASHAN PRATAP SINGH <ashanpratapsingh05@gmail.com>
2024-10-07 15:40:56 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -17,7 +17,7 @@ class Solution(object):
         total = 0
        prev_value = 0
 
-        for i in range(length):
+        for char  in range(length):
             current_value = roman_map[s[i]]
 
             # If next value exists and is larger, subtract the current value
