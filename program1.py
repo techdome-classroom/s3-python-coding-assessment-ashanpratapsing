51fc697a39a7b4e2b80346dc379e7b828d1bd820
ASHAN PRATAP SINGH <ashanpratapsingh05@gmail.com>
2024-10-07 15:47:45 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,5 +1,34 @@
 
+# program1.py
 
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # Stack to keep track of opening brackets
+        stack = []
+        
+        # Dictionary to map closing brackets to their corresponding opening brackets
+        bracket_map = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+        
+        # Traverse each character in the input string
+        for char in s:
+            # If the character is a closing bracket
+            if char in bracket_map:
+                # Pop the top element from the stack (or a dummy value if the stack is empty)
+                top_element = stack.pop() if stack else '#'
+                
+                # Check if the top element of the stack matches the corresponding opening bracket
+                if bracket_map[char] != top_element:
+                    return False
+            else:
+                # If it's an opening bracket, push it onto the stack
+                stack.append(char)
+        
+        # If the stack is empty, all brackets were matched properly
+        return not stack
 
 
 
