433dd3b5b76b7a6ea5d2c6d18896ab683a776eac
ASHAN PRATAP SINGH <ashanpratapsingh05@gmail.com>
2024-10-07 15:37:08 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -4,7 +4,28 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
+        roman_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
 
+        total = 0
+        length = len(s)
+
+        for i in range(length):
+            current_value = roman_map[s[i]]
+
+            # If next value exists and is larger, subtract the current value
+            if i + 1 < length and roman_map[s[i + 1]] > current_value:
+                total -= current_value
+            else:
+                total += current_value
+
+        return total
 
 
